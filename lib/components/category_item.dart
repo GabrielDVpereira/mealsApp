651832446ffb36fbca9a3cf194405234810299ca
Gabriel Davi <gdavi.gd@gmail.com>
2020-09-25 22:15:15 -0300

    Add navigation

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 import '../models/category.dart';
+import '../screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   CategoryItem(this.category);
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).pushNamed('/categories-meals', arguments: category);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Text(category.title, style: Theme.of(context).textTheme.headline6),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [category.color.withOpacity(0.5), category.color],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).primaryColor,
+      onTap: () => _selectCategory(context),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child:
+            Text(category.title, style: Theme.of(context).textTheme.headline6),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [category.color.withOpacity(0.5), category.color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/categories_screen.dart';
+import 'screens/categories_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,20 +8,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.amber,
-        fontFamily: 'Raleway',
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        textTheme: ThemeData.light().textTheme.copyWith(
-              headline6: TextStyle(
-                fontSize: 20,
-                fontFamily: 'RobotoCondensed',
+        title: 'DeliMeals',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          fontFamily: 'Raleway',
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                ),
               ),
-            ),
-      ),
-      home: CategoriesScreen(),
-    );
+        ),
+        home: CategoriesScreen(),
+        routes: {
+          "/categories-meals": (ctx) => CategoriesMealsScreen(),
+        });
   }
 }
